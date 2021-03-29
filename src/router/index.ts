0172098aabb17c1393bb7c8d15262676076da1a2
corentinft <45195613+corentinft@users.noreply.github.com>
2021-03-29 15:12:11 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,53 +1,67 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import {createRouter, createWebHistory} from '@ionic/vue-router';
+import {RouteRecordRaw} from 'vue-router';
 import TabsAuth from '../views/Auth/TabsAuth.vue';
 import TabsHome from '../views/App/TabsHome.vue';
-import ScanQr from '../components/ScanQr.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/auth/login'
-  },
-  {
-    path: '/auth/',
-    component: TabsAuth,
-    children: [
-      {
-        path: '',
+    {
+        path: '/',
         redirect: '/auth/login'
-      },
-      {
-        path: 'login',
-        component: () => import('@/views/Auth/Connection.vue')
-      },
-      {
-        path: 'register',
-        component: () => import('@/views/Auth/Register.vue')
-      }
-    ]
-  }
-  ,
-  {
-    path: '/App',
-    component: TabsHome,
-    children: [
-      {
-        path: 'Promotions',
-        component: () => import('@/views/App/Promotions.vue')
-      }
-    ]
-  }
-  ,
-  {
-    path: '/scan',
-    component: ScanQr
-  }
+    },
+    {
+        path: '/auth/',
+        component: TabsAuth,
+        children: [
+            {
+                path: '',
+                redirect: '/auth/login'
+            },
+            {
+                path: 'login',
+                name: 'login',
+                component: () => import('@/views/Auth/Connection.vue')
+            },
+            {
+                path: 'register',
+                name: 'register',
+                component: () => import('@/views/Auth/Register.vue')
+            }
+        ]
+    },
+    {
+        path: '/app',
+        component: TabsHome,
+        children: [
+            {
+                path: 'promotions',
+                name: 'promotions',
+                component: () => import('@/views/App/Promotions.vue')
+            },
+            {
+                path: 'scan',
+                name: 'scan',
+                component: () => import('@/views/App/Scan.vue')
+            }
+            ,
+            {
+                path: 'coupons',
+                name: 'coupons',
+                component: () => import('@/views/App/Coupon.vue')
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'login' && to.name !== 'register' && !localStorage.getItem('token')) {
+        next({name: 'login'});
+    }
+    next();
 })
 
 export default router
