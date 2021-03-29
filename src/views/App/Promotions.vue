0bdab86c03d5a8d8e930f79ae3a501bac34801c9
corentinft <45195613+corentinft@users.noreply.github.com>
2021-03-29 15:13:29 +0200

    Update Promotions.vue

--- src/views/App/Promotions.vue
+++ src/views/App/Promotions.vue
@@ -1,7 +1,11 @@
 <template>
   <ion-app>
-    <ion-header translucent>
+
+    <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="disconnect">Déconnexion</ion-button>
+        </ion-buttons>
         <ion-title>Promotions 💰</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -12,53 +16,59 @@
           <h1 class="test">Promotions disponible</h1>
         </ion-list-header>
 
-        <ion-item @click="SupressionAlert" v-for="item in $store.state.info" :key="item.id">
+        <ion-item @click="SupressionAlert(item.description)" v-for="item in $store.state.info" :key="item.id">
 
-            <ion-avatar slot="start">
-              <h1 class="h1Emoji">{{item.emoji}}</h1>
-            </ion-avatar>
-            <ion-label>
-              <h1> {{item.title}} </h1>
-              <h3>Description : {{item.description}}</h3>
-              <h3>Réductions : {{item.discount}} %</h3>
+          <ion-avatar slot="start">
+            <h1 class="h1Emoji">{{ item.emoji }}</h1>
+          </ion-avatar>
+          <ion-label>
+            <h1> {{ item.title }} </h1>
 
+            <h3>Description : {{ item.description }}</h3>
+            <h3>Réductions : {{ item.discount }} %</h3>
+            <h3>Code : {{ item.code_promo }}</h3>
 
-              <p>jusqu'au : {{ new Date(item.valid_time).toLocaleDateString('fr', {year: 'numeric', month: 'long', day: 'numeric'}) }} </p>
-            </ion-label>
-          </ion-item>
-
+            <p>jusqu'au : {{ new Date(item.valid_time).toLocaleDateString('fr', {year: 'numeric', month: 'long', day: 'numeric'}) }} </p>
+          </ion-label>
+        </ion-item>
       </ion-list>
+
     </ion-content>
   </ion-app>
 </template>
 
 <script>
-import { alertController } from '@ionic/vue';
+import {alertController, toastController} from '@ionic/vue';
+import router from "@/router";
 export default {
   name: "Promotions",
-
   beforeMount() {
     this.$store.dispatch('getPromos')
   },
-
   methods: {
-    async SupressionAlert() {
-      let item = "";
-      const Foreach = this.$store.state.info.forEach(element => item = element)
+    async SupressionAlert(description) {
       const alert = await alertController.create({
-            header: 'Description',
-            message: item.description,
-            buttons: ['Ok'],
-          });
+        header: 'Description',
+        message: description,
+        buttons: ['Ok'],
+      });
       return alert.present();
     },
+    disconnect() {
+      router.push('/auth/login');
+      localStorage.clear();
+      this.openToastDisconnect();
+    },
+    async openToastDisconnect() {
+      const toast = await toastController
+          .create({
+            header: 'Vous avez été déconnecté',
+            position: 'top',
+            duration: 5000
+          })
+      return toast.present();
+    }
   }
-  //Si bouton refresh
-  //methods: {
-    //getPromos() {
-      //this.$store.dispatch('getPromos')
-    //}
-  //}
 }
 </script>
 
